95ca2f68deccd0f59dbd088e7d9fadb951b3022f
sarahmahmoud97 <sarahmahmoud97@hotmail.com>
2017-04-14 14:32:13 +0300

    5th commit

---
+++ regstrdb.php
@@ -0,0 +1,17 @@
+<?php
+
+$user='root';
+$password='anabakrah7ali';
+$dbname='registerationdb';
+$db= new mysqli('localhost',$user,$password,$dbname) or die("Unable to connect");
+
+echo "Great WOrk";
+
+
+?>
+/**
+ * Created by PhpStorm.
+ * User: GoogleTech
+ * Date: 3/23/2017
+ * Time: 2:08 AM
+ */
